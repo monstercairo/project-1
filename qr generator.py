90569159c4c1a6c1520861b70d24c6360ec07589
nufail ahamed <ahnufail@gmail.com>
2023-04-20 14:05:46 +0530

    first commit

---
+++ qr generator.py
@@ -0,0 +1,16 @@
+import qrcode
+import image
+
+qr = qrcode.QRCode(
+    version = 15,
+    box_size = 5,
+    border = 5
+    )
+
+data = "https://www.hackthebox.com/"  # paste your link address to make QR code
+
+
+qr.add_data(data)
+qr.make(fit= True)
+img = qr.make_image(fill="black",back_color = "white")
+img.save ("test.png")
